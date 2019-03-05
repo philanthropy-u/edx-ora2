2ff5fcfd2c57af6f5999412fd60dc76694a8760a
Sadan <sadanarshad@gmail.com>
2019-03-05 12:43:55 +0500

    LP-984 - Updated script and removed end date check

--- openassessment/management/commands/autoscore_learners.py
+++ openassessment/management/commands/autoscore_learners.py
@@ -40,101 +40,98 @@ class Command(BaseCommand):
 
         for overview in course_overviews:
             course_id = overview.id
-            course_end_date = overview.end
             course_name = overview.display_name
 
-            # If course end date has met
-            if course_end_date and course_end_date <= en_date:
-                try:
-                    course_struct = CourseStructure.objects.get(
-                        course_id=course_id
-                    ).structure
-                except CourseStructure.DoesNotExist:
-                    logger.error("Course doesn't have a proper structure.")
-                    raise
+            try:
+                course_struct = CourseStructure.objects.get(
+                    course_id=course_id
+                ).structure
+            except CourseStructure.DoesNotExist:
+                logger.error("Course doesn't have a proper structure.")
+                raise
 
-                ora_blocks = []
-                for k, v in course_struct['blocks'].iteritems():
-                    if v['block_type'] == 'openassessment':
-                        ora_blocks.append(course_struct['blocks'][k])
+            ora_blocks = []
+            for k, v in course_struct['blocks'].iteritems():
+                if v['block_type'] == 'openassessment':
+                    ora_blocks.append(course_struct['blocks'][k])
 
-                # If course doesn't have any ORA blocks, continue.
-                if not ora_blocks:
-                    continue
+            # If course doesn't have any ORA blocks, continue.
+            if not ora_blocks:
+                continue
 
-                logger.info(
-                    u"Auto scoring {course_name} learners if there are any!".format(
-                        course_name=course_name
-                    )
+            logger.info(
+                u"Auto scoring {course_name} learners if there are any!".format(
+                    course_name=course_name
+                )
+            )
+
+            for ora in ora_blocks:
+                # Get all students in waiting state
+                usage_key = ora['usage_key']
+
+                waiting_students = get_users_who_not_assessed(
+                    course_id=course_id.to_deprecated_string(),
+                    item_id=usage_key
                 )
 
-                for ora in ora_blocks:
-                    # Get all students in waiting state
-                    usage_key = ora['usage_key']
+                for student in waiting_students:
+                    user = User.objects.get(id=student['id'])
+                    anonymous_user = user.anonymoususerid_set.get(course_id=course_id)
+                    student['anonymous_user_id'] = anonymous_user.anonymous_user_id
 
-                    waiting_students = get_users_who_not_assessed(
+                    # Find the associated rubric for that course_id & item_id
+                    rubric_dict = self._get_rubric_for_course(course_id, usage_key)
+
+                    rubric = rubric_from_dict(rubric_dict)
+                    options_selected, earned, possible = self._select_options(rubric_dict)
+
+                    # Use usage key and student id to get the submission of the user.
+                    try:
+                        submission = Submission.objects.get(
+                            student_item__course_id=course_id.to_deprecated_string(),
+                            student_item__student_id=student['anonymous_user_id'],
+                            student_item__item_id=usage_key,
+                            student_item__item_type='openassessment'
+                        )
+                    except Submission.DoesNotExist:
+                        logger.warn(u"No submission found for user {user_id}".format(
+                            user_id=student['anonymous_user_id']
+                        ))
+                        continue
+
+                    # Create assessments
+                    assessment = Assessment.create(
+                        rubric=rubric,
+                        scorer_id=bot_user_id,
+                        submission_uuid=submission.uuid,
+                        score_type='ST'
+                    )
+                    AssessmentPart.create_from_option_names(
+                        assessment=assessment,
+                        selected=options_selected
+                    )
+
+                    logger.info(
+                        u"Created assessment for user {user_id}, submission {submission}, "
+                        u"course {course_id}, item {item_id} with rubric {rubric} by PhilU Bot.".format(
+                            user_id=student['anonymous_user_id'],
+                            submission=submission.uuid,
+                            course_id=course_id.to_deprecated_string(),
+                            item_id=usage_key,
+                            rubric=rubric.content_hash
+                        )
+                    )
+
+                    reset_score(
+                        student_id=student['anonymous_user_id'],
                         course_id=course_id.to_deprecated_string(),
                         item_id=usage_key
                     )
-
-                    for student in waiting_students:
-                        user = User.objects.get(id=student['id'])
-                        anonymous_user = user.anonymoususerid_set.get(course_id=course_id)
-                        student['anonymous_user_id'] = anonymous_user.anonymous_user_id
-
-                        # Find the associated rubric for that course_id & item_id
-                        rubric_dict = self._get_rubric_for_course(course_id, usage_key)
-
-                        rubric = rubric_from_dict(rubric_dict)
-                        options_selected, earned, possible = self._select_options(rubric_dict)
-
-                        # Use usage key and student id to get the submission of the user.
-                        try:
-                            submission = Submission.objects.get(
-                                student_item__course_id=course_id.to_deprecated_string(),
-                                student_item__student_id=student['anonymous_user_id'],
-                                student_item__item_id=usage_key,
-                                student_item__item_type='openassessment'
-                            )
-                        except Submission.DoesNotExist:
-                            logger.warn(u"No submission found for user {user_id}".format(
-                                user_id=student['anonymous_user_id']
-                            ))
-                            continue
-
-                        # Create assessments
-                        assessment = Assessment.create(
-                            rubric=rubric,
-                            scorer_id=bot_user_id,
-                            submission_uuid=submission.uuid,
-                            score_type='ST'
-                        )
-                        AssessmentPart.create_from_option_names(
-                            assessment=assessment,
-                            selected=options_selected
-                        )
-
-                        logger.info(
-                            u"Created assessment for user {user_id}, submission {submission}, "
-                            u"course {course_id}, item {item_id} with rubric {rubric} by PhilU Bot.".format(
-                                user_id=student['anonymous_user_id'],
-                                submission=submission.uuid,
-                                course_id=course_id.to_deprecated_string(),
-                                item_id=usage_key,
-                                rubric=rubric.content_hash
-                            )
-                        )
-
-                        reset_score(
-                            student_id=student['anonymous_user_id'],
-                            course_id=course_id.to_deprecated_string(),
-                            item_id=usage_key
-                        )
-                        set_score(
-                            submission_uuid=submission.uuid,
-                            points_earned=earned,
-                            points_possible=possible
-                        )
+                    set_score(
+                        submission_uuid=submission.uuid,
+                        points_earned=earned,
+                        points_possible=possible
+                    )
 
     def _get_philu_bot(self):
         # Check if bot user exist
