e0bce9178575245392a98387065d44b57918d14a
Saad Waqar <saad.waqar@arbisoft.com>
2019-10-15 00:04:15 +0500

    course structure issues resolved

--- openassessment/management/commands/autoscore_learners.py
+++ openassessment/management/commands/autoscore_learners.py
@@ -10,6 +10,7 @@ from openedx.core.djangoapps.content.course_overviews.models import CourseOvervi
 from openedx.core.djangoapps.content.course_structures.models import CourseStructure
 from pytz import utc
 from openedx.features.assessment.helpers import autoscore_ora
+from common.djangoapps.philu_commands.helpers import generate_course_structure
 
 logger = logging.getLogger(__name__)
 
@@ -34,13 +35,11 @@ class Command(BaseCommand):
             course_id = overview.id
             course_name = overview.display_name
 
-            try:
-                course_struct = CourseStructure.objects.get(
-                    course_id=course_id
-                ).structure
-            except CourseStructure.DoesNotExist:
-                logger.error("Course doesn't have a proper structure.")
-                raise
+            course_struct = generate_course_structure(course_id)['structure']
+
+            if not course_struct:
+                logger.error('Course doesn\'t have a proper structure.')
+                continue
 
             ora_blocks = []
             for k, v in course_struct['blocks'].iteritems():
