9b6e752de400e5ebd61cf42143e68280fd61ffd8
qasimgulzar <qasim.gulzar@arbisoft.com>
2020-02-11 14:32:59 +0500

    [COMPLETE][LP-1773] Add option for table field to open assessment block. [REFACTORING] overrider oa_container.js in separate file.

--- openassessment/xblock/static/js/src/studio/oa_container.js
+++ openassessment/xblock/static/js/src/studio/oa_container.js
@@ -156,49 +156,6 @@ OpenAssessment.Container.prototype = {
         handlerItem.addHandler();
     },
     /**
-     * Adds a new table type prompt.
-     * **/
-    addTable: function () {
-        var tablePrompt = $(this.templateTablePromptElement)
-            .children().first()
-            .clone()
-            .removeAttr('id')
-            .toggleClass('is--hidden', false)
-            .toggleClass(this.containerItemClass, true)
-            .appendTo($(this.containerElement));
-
-        var tableConfiguration = {
-            change: function (table) {
-                var html = table.html();
-                tablePrompt.find('.openassessment_prompt_html_content').val(html);
-            }
-        }
-
-        var table = $(tablePrompt).oraTableBuilder(tableConfiguration);
-
-        // Since we just added the new element to the container,
-        // it should be the last one.
-        var container = this;
-        var containerItem = $("." + this.containerItemClass, this.containerElement).last();
-
-        // Install a click handler for the delete button
-        if (this.addRemoveEnabled) {
-            containerItem.find('.' + this.removeButtonClass)
-                .click(function(eventData) {
-                    var containerItem = container.createContainerItem(eventData.target);
-                    container.remove(containerItem);
-                });
-        } else {
-            containerItem.find('.' + this.removeButtonClass).addClass('is--disabled');
-        }
-
-        // Initialize the item, allowing it to install event handlers.
-        // Fire event handler for adding a new element
-        var handlerItem = container.createContainerItem(containerItem);
-        handlerItem.addEventListeners();
-        handlerItem.addHandler();
-    },
-    /**
     Remove the item associated with an element.
     If the element is not itself an item, traverse up the
     DOM tree until an item is found.
--- openassessment/xblock/studio_mixin.py
+++ openassessment/xblock/studio_mixin.py
@@ -72,6 +72,7 @@ class StudioMixin(object):
             self.add_javascript_files(fragment, "static/js/src/oa_shared.js")
             self.add_javascript_files(fragment, "static/js/src/oa_server.js")
             self.add_javascript_files(fragment, "static/js/src/studio")
+            self.add_javascript_files(fragment, "static/js/philu/studio")
         else:
             # TODO: switch to add_javascript_url once XBlock resources are loaded from the CDN
             fragment.add_javascript(pkg_resources.resource_string(__name__, "static/js/openassessment-studio.min.js"))
